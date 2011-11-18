57191591aa6e0033d7b50d3b987ceec602a677e2
Carter Corker <cartercorker@gmail.com>
2011-11-18 08:26:52 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var scottsafety = {
+	
+	autoPlayTimerStart : function () {
+		
+	}
+	
+}
---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Scott Safety</title>
+
+<script type="text/javascript" src="jquery.js"></script>
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+    <body>
+    	<div id="wrapper">
+    		<div id="body-container">
+    			<div id="header-container">
+    				Navbar
+    			</div>
+    			<div id="top-container">
+    				<div id="hero-container">
+    					<div class="hero-content-container">
+    						<div class="grouping-wrapper">
+    							<div class="hero-title-container">
+	    							<h1 class="hero-title">
+	    								Search and Rescue
+	    							</h1>
+	    							<span class="hero-tagline">
+	    								Discover rescue products and more
+	    							</span>
+	    						</div>
+	    						<div class="hero-image-container">
+	    							<div class="grouping-wrapper">
+	    								<div class="image-back"></div>
+	    								<div class="image-fore"></div>
+	    							</div>
+	    						</div>
+    						</div>
+    					</div>
+    				</div>
+    				<div id="red-bar">
+    					<div id="red-bar-content-container">
+    						<span class="play-button">Play</span>
+    					</div>
+    				</div>
+    			</div>
+    			<div id="bottom-container">
+    				<div id="carousel">
+    					
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    	
+		<script type="text/javascript" src="index.js"></script>
+		<script>
+		$(document).ready(function () {
+			
+		});
+		</script>
+    </body>
+</html>
